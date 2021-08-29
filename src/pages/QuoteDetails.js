d49349536ce7ea9a49f7b09f2ef88580f6963237
Cristovao Neto <cristovaon13@gmail.com>
2021-08-28 21:50:54 -0300

    max's version of the project

--- src/pages/QuoteDetails.js
+++ src/pages/QuoteDetails.js
@@ -2,28 +2,45 @@ import { Route, useParams, useRouteMatch } from 'react-router';
 import { Link } from 'react-router-dom';
 import Comments from '../components/comments/Comments';
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
-import useQuotes from '../hooks/use-quotes';
+import useHttp from '../hooks/use-http';
+import { getSingleQuote } from '../lib/api';
+import { useEffect } from 'react';
+import LoadingSpinner from '../components/UI/LoadingSpinner';
 
 export default function QuoteDetails() {
   const params = useParams();
   const match = useRouteMatch();
   const quoteID = params.id;
-  const { quotes } = useQuotes();
-  console.log(quoteID);
 
-  const quote = quotes.find(quote => quote.id === quoteID);
+  const {
+    sendRequest,
+    status,
+    data: loadedQuote,
+    error,
+  } = useHttp(getSingleQuote, true);
+  useEffect(() => {
+    sendRequest(quoteID);
+  }, [sendRequest, quoteID]);
 
-  if (!quote) {
+  if (status === 'pending') {
     return (
-      <div className="error-msg">
-        <h2>No quote found.</h2>
+      <div className="centered">
+        <LoadingSpinner />
       </div>
     );
   }
 
+  if (error) {
+    return <p className="centered">{error}</p>;
+  }
+
+  if (!loadedQuote.text) {
+    return <p>No quote found.</p>;
+  }
+
   return (
     <section>
-      <HighlightedQuote author={quote.author} text={quote.text} />
+      <HighlightedQuote author={loadedQuote.author} text={loadedQuote.text} />
       {/* Condicional rendering bases on the URL, so we don't need to manage complex state */}
       <Route exact path={match.path}>
         <div className="centered">
